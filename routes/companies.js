80bb159bb0532a7e98dbd082d64adcb465587430
David Sapiro <dsap48@gmail.com>
2024-03-21 12:03:12 -0700

    added get company and post new company routes

--- routes/companies.js
+++ routes/companies.js
@@ -4,15 +4,38 @@ const { BadRequestError, NotFoundError } = require('../expressError');
 
 const router = new express.Router();
 
-router.get('/', async function(req, res) {
+router.get('/', async function (req, res) {
   const results = await db.query(
     `SELECT code, name
     FROM companies;`
   );
 
   const companies = results.rows;
-  return res.json({companies});
-})
+  return res.json({ companies });
+});
+
+router.get('/:code', async function (req, res) {
+  const results = await db.query(
+    `SELECT code, name, description
+    FROM companies
+    WHERE code = $1`, [req.params.code]
+  );
+
+  const company = results.rows[0];
+  return res.json({company});
+});
+
+router.post('/', async function(req, res) {
+  const { code, name, description } = req.body;
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`, [code, name, description]
+  );
+
+  const company = results.rows[0];
+  return res.json({company});
+});
 
 
 module.exports = router;
